b4bc3bce71cc7c6df37b3636345a14a8443233b8
Nicolás López <nicolaslopezj@me.com>
2015-12-08 15:24:52 -0300

    colores

---
+++ orion/dictionary/colors.js
@@ -0,0 +1,55 @@
+orion.dictionary.addDefinition('alt', 'colors', {
+  type: String,
+  label: 'Principal',
+  autoform: {
+    type: 'color'
+  }
+});
+
+orion.dictionary.addDefinition('header', 'colors', {
+  type: String,
+  label: 'Header',
+  autoform: {
+    type: 'color',
+  }
+});
+
+orion.dictionary.addDefinition('features', 'colors', {
+  type: String,
+  label: 'Features',
+  autoform: {
+    type: 'color',
+  }
+});
+
+orion.dictionary.addDefinition('tickets', 'colors', {
+  type: String,
+  label: 'Tickets',
+  autoform: {
+    type: 'color',
+  }
+});
+
+orion.dictionary.addDefinition('barra', 'colors', {
+  type: String,
+  label: 'Barra',
+  autoform: {
+    type: 'color',
+  }
+});
+
+orion.dictionary.addDefinition('inter', 'colors', {
+  type: String,
+  label: 'Intermedio',
+  autoform: {
+    type: 'color',
+  }
+});
+
+orion.dictionary.addDefinition('contacto', 'colors', {
+  type: String,
+  label: 'Contacto',
+  autoform: {
+    type: 'color',
+  }
+});
